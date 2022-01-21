17eae2430330e55f444cb868908e8e193f07b42d
JULIO EMMANUEL VALLE CALAM <jvallec@frd.org.mx>
2022-01-21 10:32:44 -0600

    Get all people

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const people = require("./mockData/peopleData");
+
+app.use(express.json());
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
 
 app.listen(5000, () => {
   console.log("App listening on port 5000");
