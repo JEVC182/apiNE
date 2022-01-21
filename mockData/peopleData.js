f9b2d5edc867c8874e2fb0a05476344a5b8038fb
JULIO EMMANUEL VALLE CALAM <jvallec@frd.org.mx>
2022-01-21 10:26:01 -0600

    Added mock data

---
+++ mockData/peopleData.js
@@ -0,0 +1,124 @@
+const people = [
+  {
+    id: 1,
+    first_name: "Kissie",
+    last_name: "Down",
+    email: "kdown0@moonfruit.com",
+  },
+  {
+    id: 2,
+    first_name: "Francisca",
+    last_name: "Shirrell",
+    email: "fshirrell1@com.com",
+  },
+  {
+    id: 3,
+    first_name: "Bogart",
+    last_name: "Sheldrake",
+    email: "bsheldrake2@nih.gov",
+  },
+  {
+    id: 4,
+    first_name: "Byron",
+    last_name: "Bradtke",
+    email: "bbradtke3@1und1.de",
+  },
+  {
+    id: 5,
+    first_name: "Sheelah",
+    last_name: "Scoon",
+    email: "sscoon4@phpbb.com",
+  },
+  {
+    id: 6,
+    first_name: "Kellby",
+    last_name: "Jeffcoate",
+    email: "kjeffcoate5@xrea.com",
+  },
+  {
+    id: 7,
+    first_name: "Denny",
+    last_name: "Ellett",
+    email: "dellett6@wiley.com",
+  },
+  {
+    id: 8,
+    first_name: "Aridatha",
+    last_name: "Tiernan",
+    email: "atiernan7@ocn.ne.jp",
+  },
+  {
+    id: 9,
+    first_name: "Deck",
+    last_name: "Beasant",
+    email: "dbeasant8@phoca.cz",
+  },
+  {
+    id: 10,
+    first_name: "Adolphus",
+    last_name: "Linder",
+    email: "alinder9@mediafire.com",
+  },
+  {
+    id: 11,
+    first_name: "Jennilee",
+    last_name: "Scurrer",
+    email: "jscurrera@admin.ch",
+  },
+  {
+    id: 12,
+    first_name: "Brynne",
+    last_name: "Sell",
+    email: "bsellb@cbc.ca",
+  },
+  {
+    id: 13,
+    first_name: "Timofei",
+    last_name: "Mumm",
+    email: "tmummc@zimbio.com",
+  },
+  {
+    id: 14,
+    first_name: "Caroljean",
+    last_name: "Devoy",
+    email: "cdevoyd@is.gd",
+  },
+  {
+    id: 15,
+    first_name: "Idette",
+    last_name: "Northall",
+    email: "inorthalle@homestead.com",
+  },
+  {
+    id: 16,
+    first_name: "Susette",
+    last_name: "Padfield",
+    email: "spadfieldf@state.gov",
+  },
+  {
+    id: 17,
+    first_name: "Igor",
+    last_name: "Rowan",
+    email: "irowang@un.org",
+  },
+  {
+    id: 18,
+    first_name: "Noe",
+    last_name: "Rathke",
+    email: "nrathkeh@behance.net",
+  },
+  {
+    id: 19,
+    first_name: "Daisey",
+    last_name: "Flint",
+    email: "dflinti@oracle.com",
+  },
+  {
+    id: 20,
+    first_name: "Mauricio",
+    last_name: "Epgrave",
+    email: "mepgravej@yandex.ru",
+  },
+];
+
+module.exports = people;
