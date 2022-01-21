9f444378c932b6c2bc7c6cc9d511ef3f6fee3abd
JULIO EMMANUEL VALLE CALAM <jvallec@frd.org.mx>
2022-01-21 11:00:16 -0600

    Added update function

--- app.js
+++ app.js
@@ -17,6 +17,24 @@ app.get("/api/people/:id", (req, res) => {
   }
   res.json({ success: true, data: singlePerson });
 });
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { email } = req.body;
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    return res.status(404).json({
+      success: false,
+      message: `Could not found a user with id ${id}`,
+    });
+  }
+  const updatedPerson = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.email = email;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: updatedPerson });
+});
 app.listen(5000, () => {
   console.log("App listening on port 5000");
 });
